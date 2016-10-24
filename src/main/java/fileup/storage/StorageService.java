44cbabbebeb1aa7bda8ced035b73d61fca04186f
kakonss <kakonfriend@gmail.com>
2016-10-24 17:21:52 +0800

    First COmmit

---
+++ src/main/java/fileup/storage/StorageService.java
@@ -0,0 +1,12 @@
+package fileup.storage;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+
+	void init();
+
+    void store(MultipartFile file);
+    
+    void deleteAll();
+}
